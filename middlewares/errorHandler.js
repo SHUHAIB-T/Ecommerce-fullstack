8e6027f0fe7a4801f2ed75c9dfe6c3ab8b8c9e86
Shuhaib T U <tushuhaib404@gmail.com>
2023-10-03 12:42:03 +0530

    404 page added

--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -1,16 +1,12 @@
 const notFound = (req, res, next) => {
-    const error = new Error(`Not Found - ${req.originalUrl}`);
-    res.status(404);
-    next(error);
-  }
+  const error = new Error(`Not Found - ${req.originalUrl}`);
+  res.status(404).render('error/404', { user: true,});
+  next(error);
+}
 
 const errorHandler = (err, req, res, next) => {
-    const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
-    res.status(statusCode);
-    res.json({
-      message: err?.message,
-      stack: err?.stack,
-    });
-  }
+  const statusCode = res.statusCode === 200 ? 500 : res.statusCode;
+  res.status(statusCode);
+}
 
-  module.exports = {notFound, errorHandler}
+module.exports = { notFound, errorHandler }
